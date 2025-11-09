877c4f93202f481faa0cc15c5253a3d93a1f9ecb
lare <lare354@duck.com>
2025-11-09 01:01:30 +0100

    asd

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+localBin=$HOME/.local/bin
+
 mkdir .tmp
 cd .tmp
 git clone https://github.com/lare354/waller
 cd waller
 
+cp waller $localBin/waller
+
+if [ ! -f $localBin/waller ]; then
+	printf 'Failed to move waller to $HOME/.local/bin/'
+	return 0
+else
+	printf 'Success'
+fi
